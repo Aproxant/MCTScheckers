7e96fc5de96c73244bb5fc1b8589ff70d9556af2
Jeremi Kurdek <jkurdek@gmail.com>
2023-06-08 17:16:45 +0200

    added test script

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define a function that runs the Python script and increments the counters
+run_checker() {
+    # Launch the checker.py program
+    output=$(python3 checkers.py | tail -n 1)
+
+    # Check the output and increment the appropriate counter
+    if [[ $output == "RED WINS!" ]]
+    then
+        echo "red"
+    elif [[ $output == "BLUE WINS!" ]]
+    then
+        echo "blue"
+    fi
+}
+
+export -f run_checker
+
+# Use GNU parallel to run the function 100 times
+results=$(parallel -j 10 run_checker ::: {1..100})
+
+# Count the red and blue wins
+red_wins=$(echo "$results" | grep -c "red")
+blue_wins=$(echo "$results" | grep -c "blue")
+
+# Print the statistics
+echo "Total Red Wins: $red_wins"
+echo "Total Blue Wins: $blue_wins"
